1cdf9d0de6a4b82bd376beeb669b69da70365710
haocheng <haocheng.lee@gmail.com>
2013-08-03 17:12:04 +0800

    move chrome/firefox folders to other partition to save space

---
+++ createSoftLinks.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+ln -s ~/data/app/Genymotion_Home ~/.Genymobile/Genymotion
+
+## for Chrome
+ln -s ~/data/app/Chrome/google-chrome ~/.config/google-chrome
+ln -s ~/data/app/Chrome/cache ~/.cache/google-chrome
+
+## for Firefox
+ln -s ~/data/app/Firefox/mozilla ~/.mozilla
+ln -s ~/data/app/Firefox/cache ~/.cache/mozilla
