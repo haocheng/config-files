c2ca1cfebb7c047506362a33a487e49ae6842b99
haocheng <haocheng.lee@gmail.com>
2014-08-15 14:50:46 +0800

    add less syntax highlight in fish

--- fish/config.fish
+++ fish/config.fish
@@ -13,14 +13,18 @@ set fish_plugins tmux
 #set fish_custom $HOME/dotfiles/oh-my-fish
 
 ## set env variables
-set JAVA_HOME /usr/lib/jvm/java-8
-set GRADLE_HOME ~/develop/gradle
-set ANDROID_HOME ~/develop/android-sdk-linux_x86
-set ANT_HOME ~/develop/ant
-set M2_HOME ~/develop/maven3
-set GRADLE_USER_HOME ~/develop/gradle-home
-set DART_SDK ~/develop/dart/dart-sdk
-set MAVEN_OPTS "-Xmx256m -XX:MaxPermSize=128m"
+set -x JAVA_HOME /usr/lib/jvm/java-8
+set -x GRADLE_HOME ~/develop/gradle
+set -x ANDROID_HOME ~/develop/android-sdk-linux_x86
+set -x ANT_HOME ~/develop/ant
+set -x M2_HOME ~/develop/maven3
+set -x GRADLE_USER_HOME ~/develop/gradle-home
+set -x DART_SDK ~/develop/dart/dart-sdk
+set -x MAVEN_OPTS "-Xmx256m -XX:MaxPermSize=128m"
+
+## for less syntax highlight
+set -x LESSOPEN '| /usr/share/source-highlight/src-hilite-lesspipe.sh %s'
+set -x LESS ' -R '
 
 ## set path
 set -g -x PATH $PATH /usr/bin /bin /usr/sbin /sbin /usr/local/bin $JAVA_HOME/bin $ANT_HOME/bin $M2_HOME/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/tools $GRADLE_HOME/bin $DART_SDK/bin
