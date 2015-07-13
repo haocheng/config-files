601cce3ee094abdb45b3e0818326e3ce9c767511
haocheng <haocheng.lee@gmail.com>
2015-07-13 10:52:43 +0800

    update config.fish after upgrade to latest oh-my-fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,20 @@
 # Path to your oh-my-fish.
 set fish_path $HOME/.oh-my-fish
 
-# Theme
-set fish_theme bira
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-set fish_plugins tmux
-
-# Path to your custom folder (default path is $FISH/custom)
+# Path to your custom folder (default path is ~/.oh-my-fish/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+
+# Custom plugins and themes may be added to ~/.oh-my-fish/custom
+# Plugins and themes can be found at https://github.com/oh-my-fish/
+Theme 'l'
+
+Plugin 'theme'
+Plugin 'android-sdk'
+Plugin 'emoji-clock'
+Plugin 'tmux'
 
 ## set env variables
 set -x JAVA_HOME /usr/lib/jvm/java-8
@@ -29,5 +33,3 @@ set -x LESS ' -R '
 ## set path
 set -g -x PATH $PATH /usr/bin /bin /usr/sbin /sbin /usr/local/bin $JAVA_HOME/bin $ANT_HOME/bin $M2_HOME/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/tools $GRADLE_HOME/bin $DART_SDK/bin
 
-# Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
